daebaf79538c447b7571503c0f6724a2e49a7540
aldodomano <tiano.roma@gmail.com>
2022-10-21 15:33:45 +0200

    Added .env files

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const cloudinary = require("cloudinary").v2; // On n'oublie pas le `.v2` à la fin
 
 // Données à remplacer avec les vôtres :
 cloudinary.config({
-  cloud_name: "dcb0jrjim",
-  api_key: "552938344947371",
-  api_secret: "w7r3JMeALGvDlw-CatrHpgZmrfU",
+  // cloud_name: "dcb0jrjim",
+  // api_key: "552938344947371",
+  // api_secret: "w7r3JMeALGvDlw-CatrHpgZmrfU",
+
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_KEY,
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/vinted");
+mongoose.connect(process.env.MONGODB_URI);
 
 const routeUser = require("./routes/user");
 const routeOffer = require("./routes/offer");
@@ -26,6 +33,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "Page not found" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server vinted started...");
 });
